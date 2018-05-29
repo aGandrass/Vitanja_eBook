7f08ccec9f31876d035a6b64cee1bd9ef09665a1
=Andre Gandrass <=andre.gandrass@steigenberger.com>
2018-05-29 10:09:36 +0200

    Insert Iconic Icons again

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,7 +27,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
 
     <link href="{{ asset('css/custom-css.css') }}" rel="stylesheet">
-    <!-- <link href="{{ asset('font/css/open-iconic-bootstrap.css') }}" rel="stylesheet"> ->
+    <link href="{{ asset('font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
 
 
 </head>
